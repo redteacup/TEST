5f9cfaa5f9df55398366f4b9cf02e0798a2eb27b
liveangel <jiangshu.social@gmail.com>
2015-06-29 13:52:15 +0800

    显示公告

--- app/src/main/java/com/show/NoticeActivity.java
+++ app/src/main/java/com/show/NoticeActivity.java
@@ -1,9 +1,17 @@
 package com.show;
 
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import com.example.liveangel.test.R;
 
 /**
  * Created by liveangel on 15-6-29.
  */
 public class NoticeActivity extends ActionBarActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+
+
+    }
 }
